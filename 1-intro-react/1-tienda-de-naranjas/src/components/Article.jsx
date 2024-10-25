ea4b29ff806a6d8c977d2cc2b46545c371f1b730
laupp01 <dahiana01perez@gmail.com>
2024-10-25 07:24:46 -0500

    Creación card bootstrap

---
+++ 1-intro-react/1-tienda-de-naranjas/src/components/Article.jsx
@@ -0,0 +1,7 @@
+
+
+export default function Article()  {
+    return (
+        <article>Article 😈</article>
+    )
+}
---
+++ 1-intro-react/1-tienda-de-naranjas/src/components/Section.jsx
@@ -0,0 +1,62 @@
+
+
+export default function Section() {
+  
+    return ( 
+        <>
+          <section>
+            <div className="Container">
+                <h1 className="text-warning">Sección de promos Naranjas 🍊</h1>
+                <div className="row">
+                    <div className="col">
+                        <div class="card">
+                            <img src="https://i0.wp.com/thefoodiesavenue.com/wp-content/uploads/2023/07/Oranges-mix6-1.png?resize=1024%2C701&ssl=1" className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Naranjas multicolor</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col">
+                        <div class="card">
+                            <img src="https://i0.wp.com/thefoodiesavenue.com/wp-content/uploads/2023/07/Oranges-mix6-1.png?resize=1024%2C701&ssl=1" className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Naranjas clasicas</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col">
+                        <div class="card">
+                            <img src="https://img.freepik.com/premium-photo/three-oranges-tree-with-leaves-sky-background_763111-19.jpg" className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Solo naranjas</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col">
+                        <div class="card">
+                            <img src="https://cdn-prod.medicalnewstoday.com/content/images/articles/272/272782/oranges-in-a-box.jpg" className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </section>  
+        </>
+        
+        
+    )
+  
+}
